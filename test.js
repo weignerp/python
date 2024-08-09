ce9114366cfa712606d50be26f3e1a7d0110fe40
Pavel Weigner <weignerp@gmail.com>
2024-08-09 09:09:45 +0200

    Aktualizace

--- test.js
+++ test.js
@@ -1,8 +1,26 @@
-var t = 'test';
-console.log(t);
+39: Admin Login - Login
+40: Carousel - Not adding content to other languages if a new fields group with + is added
+41: Carousel - Adding image via article
+42: List # mor than 10 - Displayed not okay
+43: Sciena Appi - Sent out to only news approved in specific folder
+44: Internal News Article -  Display not the target page as expected. Display on standard article view page
+45: Standard article - Distance after image in paragraph
+46: mediateka-plus, mediateka-tv, Permission
+47: Redirect / Forward - friends
+48: Upload document or photos - Takes to long, especially the first time.
+49: "mediateka-tv, Reporting
+50: "mediateka-tv, mediateka-plus - Funkctionality 
+51: Perfomance - Increase performance. In Tab 51 you see as an example today's status. Around August 08th we expect a better performance, as we are then on month live.
+52: "Open Graph Protocol, LinkedIn, Twitter Card etc.Microformats, social profiles, doctype HTML5, Character encoding (UTF-8?)" - Enable your website to be shared on LinkedIn and provide visitors a better media experience
+53: "SEO, Headings, web feeds, indexing of web pages, Robot.txt, sitemap (HTML / XML), videos (indexed), dead links / broken links" - Increase that Empa topics (news etc.)  are better found on the web
+54: "mediateka-tv, mediateka-plus" - Funkctionality 
+55: Social Media Icons - "Bottom Action Toolbar, Bottom Action Template"
+56: Social Media Icons - Social Media Icons in the Footer to be showed mor clear
 
-function isPrime(num) {
-    for(let i = 2, sqrt = Math.sqrt(num); i <= sqrt; i++)
-        if(num % i === 0) return false; 
-    return num > 1;
-}
+57:	Redirects
+
+58: User Group Wiki, Permission
+59: Team List Portlet, Added person not correct presented in the configuration view
+
+60: New Personal Website, Permission issue
+61: Wiki - User, Permission issue
